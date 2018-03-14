4db1ccc95c6d5827d385ec9a901596d9c7d62a6c
mMarcos208 <marconimbarroso@gmail.com>
2018-03-14 19:22:02 -0300

    ùltima aula

--- executandoRegex/quantificadores/desafioEmail.js
+++ executandoRegex/quantificadores/desafioEmail.js
@@ -2,8 +2,14 @@ const texto = `Os e-mail dos convidados são:
 -marconi@gmail.com
 -marconimbarroso@gmail.com
 -magno@gmail.com.br
--marconimbarroso@gmail.com`
+-marconimbarroso@gmailcom
+-marconi.mbarroso@gmail.com
+-marconi.mbarroso@gmail.com.br`
 
-const expressao = /.+@.+/g;
+const expressao = /\w+@\w+\.\w{2,4}/g; //1ª versão
+const expressao2 = /[\w.]+@\w+\.\w{2,4}/g; //2ª versão
+const expressao3 = /[\w.]+@\w+\.\w{2,4}\.?\w+/g; //3ª versão
 
 console.log(texto.match(expressao));
+console.log(texto.match(expressao2));
+console.log(texto.match(expressao3));
